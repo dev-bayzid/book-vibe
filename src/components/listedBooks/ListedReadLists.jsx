cf3652dea4214b0b125161dc94202e02fc98869d
Bayzid <macbook@Bayzids-MacBook-Air.local>
2026-05-06 18:13:41 +0600

    set sorting by pages and rating in read list

--- src/components/listedBooks/ListedReadLists.jsx
+++ src/components/listedBooks/ListedReadLists.jsx
@@ -1,12 +1,30 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { BookContext } from "../../context/BookProvider";
 import ListedBooksCard from "../UI/ListedBooksCard";
 
-const ListedReadLists = () => {
+const ListedReadLists = ({ sortingType }) => {
   const { storedBooks } = useContext(BookContext);
   console.log(storedBooks);
 
-  if (storedBooks.length === 0) {
+  const [filteredReadList, setFilteredReadList] = useState(storedBooks);
+
+  useEffect(() => {
+    if (sortingType) {
+      if (sortingType === "pages") {
+        const sortedData = [...storedBooks].sort(
+          (a, b) => a.totalPages - b.totalPages,
+        );
+        console.log(sortedData);
+        setFilteredReadList(sortedData);
+      } else if (sortingType === "rating" ) {
+        const sortedData = [...storedBooks].sort((a, b) => a.rating - b.rating);
+        console.log(sortedData);
+        setFilteredReadList(sortedData);
+      }
+    }
+  }, [sortingType, storedBooks]);
+
+  if (filteredReadList.length === 0) {
     return (
       <div className="flex justify-center items-center h-[50vh] rounded-3xl bg-base-300">
         <h2 className="font-bold text-3xl">No read list data found</h2>
@@ -16,7 +34,7 @@ const ListedReadLists = () => {
 
   return (
     <div>
-      {storedBooks.map((book, ind) => (
+      {filteredReadList.map((book, ind) => (
         <ListedBooksCard key={ind} book={book}></ListedBooksCard>
       ))}
     </div>
--- src/pages/Books/Books.jsx
+++ src/pages/Books/Books.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { BookContext } from "../../context/BookProvider";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
@@ -9,8 +9,29 @@ const Books = () => {
   const { storedBooks, wishList } = useContext(BookContext);
   console.log(storedBooks, wishList);
 
+  const [sortingType, setSortingType] =useState("");
+    console.log(sortingType, "sortingType");
   return (
     <div className="container mx-auto my-6">
+      <div className="flex justify-end items-center">
+        <div className="dropdown dropdown-center">
+          <div tabIndex={0} role="button" className="btn m-1">
+            Sort by
+          </div>
+          <ul
+            tabIndex="-1"
+            className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm"
+          >
+            <li onClick={() => {setSortingType('pages')}}>
+              <a>Pages</a>
+            </li>
+            <li onClick={() => {setSortingType('rating')}}> 
+              <a>Rating</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
       <Tabs>
         <TabList>
           <Tab>Read Books</Tab>
@@ -18,10 +39,10 @@ const Books = () => {
         </TabList>
 
         <TabPanel>
-          <ListedReadLists></ListedReadLists>
+          <ListedReadLists sortingType={sortingType}></ListedReadLists>
         </TabPanel>
         <TabPanel>
-          <ListedWishLists></ListedWishLists>
+          <ListedWishLists sortingType={sortingType}></ListedWishLists>
         </TabPanel>
       </Tabs>
     </div>
