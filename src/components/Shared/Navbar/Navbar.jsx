88b1c3a8e6b38872cdfb85b7bac3fcb496a25d55
Bayzid <macbook@Bayzids-MacBook-Air.local>
2026-05-06 08:30:43 +0600

    added navbar and 404 error page and apply  style

--- src/components/Shared/Navbar/Navbar.jsx
+++ src/components/Shared/Navbar/Navbar.jsx
@@ -1,4 +1,41 @@
+import { NavLink } from "react-router";
+
 const Navbar = () => {
+  const links = (
+    <>
+      <li>
+        <NavLink
+          to={"/"}
+          className={({ isActive }) =>
+            `font-semibold mr-1 ${isActive ? "text-green-600 border border-green-500" : " "}`
+          }
+        >
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to={"/books"}
+          className={({ isActive }) =>
+            `font-semibold mr-1 ${isActive ? "text-green-600 border border-green-500" : " "}`
+          }
+        >
+          Listed Books
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to={"/pages-to-go"}
+          className={({ isActive }) =>
+            `font-semibold mr-1 ${isActive ? "text-green-600 border border-green-500" : " "}`
+          }
+        >
+          Pages to Read
+        </NavLink>
+      </li>
+    </>
+  );
+
   return (
     <div className="navbar bg-base-100 shadow-sm">
       <div className="navbar-start">
@@ -24,52 +61,17 @@ const Navbar = () => {
             tabIndex="-1"
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
           >
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
+            {links}
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <h1 className="font-bold text-xl">Book Vibe</h1>
       </div>
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <a>Item 1</a>
-          </li>
-          <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2 bg-base-100 w-40 z-1">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </details>
-          </li>
-          <li>
-            <a>Item 3</a>
-          </li>
-        </ul>
+        <ul className="menu menu-horizontal px-1">{links}</ul>
       </div>
-      <div className="navbar-end">
-        <a className="btn">Button</a>
+      <div className="navbar-end gap-2">
+        <button className="btn btn-success text-white">Sign In</button>
+        <button className="btn btn-info text-white">Sign Up</button>
       </div>
     </div>
   );
