24c6e6cb5c72d5ad3085290904fd67a41b6059d3
Bayzid <macbook@Bayzids-MacBook-Air.local>
2026-05-06 18:19:23 +0600

    set sorting by pages and rating in wish list

--- src/components/listedBooks/ListedWishLists.jsx
+++ src/components/listedBooks/ListedWishLists.jsx
@@ -1,12 +1,30 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { BookContext } from "../../context/BookProvider";
 import ListedBooksCard from "../UI/ListedBooksCard";
 
-const ListedWishLists = () => {
+const ListedWishLists = ({ sortingType }) => {
   const { wishList } = useContext(BookContext);
   console.log(wishList);
 
-  if (wishList.length === 0) {
+  const [filteredWishList, setFilteredWishList] = useState(wishList);
+
+  useEffect(() => {
+    if (sortingType) {
+      if (sortingType === "pages") {
+        const sortedData = [...wishList].sort(
+          (a, b) => a.totalPages - b.totalPages,
+        );
+        console.log(sortedData);
+        setFilteredWishList(sortedData);
+      } else if (sortingType === "rating") {
+        const sortedData = [...wishList].sort((a, b) => a.rating - b.rating);
+        console.log(sortedData);
+        setFilteredWishList(sortedData);
+      }
+    }
+  }, [sortingType, wishList]);
+
+  if (filteredWishList.length === 0) {
     return (
       <div className="flex justify-center items-center h-[50vh] rounded-3xl bg-base-300">
         <h2 className="font-bold text-3xl">No wish list data found</h2>
@@ -16,7 +34,7 @@ const ListedWishLists = () => {
 
   return (
     <div>
-      {wishList.map((book, ind) => (
+      {filteredWishList.map((book, ind) => (
         <ListedBooksCard key={ind} book={book}></ListedBooksCard>
       ))}
     </div>
